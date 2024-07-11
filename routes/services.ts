3b46db4d397bc62d1f7a64ed9b6acf5e17c7536e
Mostafaa8 <mos.abdallah@yahoo.com>
2024-07-11 11:20:53 +0300

    adding services pages

--- routes/services.ts
+++ routes/services.ts
@@ -2,8 +2,16 @@ import { Router } from "express";
 const services = Router()
 
 services.get('/',(req,res)=>{
-    
+    res.render('services.ejs');
 })
-
+services.get('/catalog',(req,res)=>{
+    res.render('catalog.ejs');
+})
+services.get('/robot',(req,res)=>{
+    res.render('robot.ejs')
+})
+services.get('/about',(req,res)=>{
+    res.render('about.ejs')
+})
 
 export default services
